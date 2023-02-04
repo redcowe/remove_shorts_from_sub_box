4468e35ae86c669eccaea4f83a7ffb4a711e952c
recowe <joshuacowell@hotmail.com>
2023-02-04 18:18:16 -0500

    working prototype done

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
-chrome.tabs.onUpdated.addListener((tabId, tab, changeInfo) => {
-    if (tab.url.indexOf("https://youtube.com/feed/subscriptions") && changeInfo.url === undefined) {
-      chrome.tabs.executeScript(tabId, {file: "contentScript.js"})
-    };
-  });
+chrome.tabs.onUpdated.addListener((tabId, info) => {
+  if (info.status = 'complete') {
+    chrome.tabs.sendMessage(tabId, {
+      tabId: tabId,
+      test: true
+  })
+  };
+})
--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,14 @@
-console.log("hi!!!");
+(() => {
+    const YOUTUBE_SHORT = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-time-status-renderer")
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const {test, tabId} = obj;
+        if (test && tabId) {
+            for (let index = 0; index < YOUTUBE_SHORT.length; index++) {
+                console.log("removing");
+                const element = YOUTUBE_SHORT[index];
+                element.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+            }
+        }
+    })
+})();
